9ddb5a32834ecf06c7c3308a83bf7441acd84883
Ridham <rasenrhino@pop-os.localdomain>
2020-08-17 23:30:31 +0530

    faster screen req

--- screen.py
+++ screen.py
@@ -17,15 +17,15 @@ while True:
 	frame = np.array(img)
 	grayscaled = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 	ret,thresh = cv2.threshold(grayscaled,140,255,cv2.THRESH_BINARY)
-	# for i in range(540,1080):
-	# 	if thresh[i,300]==0:
-	# 		if(i<670):
-	# 			print(i)
-	# 			keyboard.press(Key.space)                                      
-	# 			break
-	# 		else:
-	# 			break                          
-	cv2.imshow('lol',thresh)            
+	for i in range(540,1080):
+		if thresh[i,300]==0:
+			if(i<690):
+				print(i)
+				keyboard.press(Key.space)                                      
+				break
+			else:
+				break                          
+	# cv2.imshow('lol',thresh)            
 	# if the user clicks q, it exits
 	if cv2.waitKey(1) == ord("q"):                      
 		break
